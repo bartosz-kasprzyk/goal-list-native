0722ebb3091643cf45928511fe0c424b19ddc519
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-12-03 23:32:17 +0100

    implemented adding goals functionality

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), text: enteredGoalText },
+    ]);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputSection}>
-        <TextInput style={styles.textInput} placeholder="Your new goal" />
-          <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your new goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalList}>
-        <View style={styles.goalItem}>
-          <Text style={styles.goalText}>a</Text>
-        </View>
-        <View style={styles.goalItem}>
-          <Text style={styles.goalText}>b</Text>
-        </View>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        ></FlatList>
       </View>
     </View>
   );
@@ -24,14 +58,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     padding: 8,
-    paddingTop: 30,
+    paddingTop: 50,
   },
   inputSection: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingVertical: 16,
+    paddingBottom: 16,
     borderBottomWidth: 1,
     borderColor: "grey",
   },
@@ -42,15 +76,15 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   goalList: {
-    flex: 8,
-    paddingVertical: 8
+    flex: 6,
+    paddingVertical: 8,
   },
   goalItem: {
     backgroundColor: "purple",
     padding: 8,
-    marginVertical: 8
+    marginVertical: 8,
   },
   goalText: {
-    color: "white"
+    color: "white",
   },
 });
